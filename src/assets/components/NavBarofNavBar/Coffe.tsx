757ac3e81f06cd21913d5b50e936f2d41ae3f180
whoever-you-wanna-think <banvietthenaocungdc@gmail.com>
2023-08-28 11:11:00 +0700

    Adjust some component properties and erect some tsx file for nav bar options when we hover up and down

---
+++ src/assets/components/NavBarofNavBar/Coffe.tsx
@@ -0,0 +1,7 @@
+// import React from 'react'
+
+const Coffe = () => {
+  return <div className="nav_toggling">Hello</div>;
+};
+
+export default Coffe;
--- src/assets/components/stuffs/Nav.tsx
+++ src/assets/components/stuffs/Nav.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const Nav: React.FC = () => {
   return (
     <nav
